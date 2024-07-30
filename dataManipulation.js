7316760b0eafbb2fd3df50f9ecd9b4a5e84c2c08
init-dev17 <dev@init17.fr>
2024-07-30 11:21:49 +0200

    statsUpdate30/07

--- dataManipulation.js
+++ dataManipulation.js
@@ -3,25 +3,25 @@
 
 let scrap = `Metrics
 NUMBER OF JEWELS CIRCULATING
-46,780,481
+46,851,325
 TOTAL TIER 1 JEWELS CIRCULATION
-42,386,227
+42,455,237
 TOTAL TIER 2 JEWELS CIRCULATION
-1,704,950
+1,704,254
 TOTAL TIER 3 JEWELS CIRCULATION
-2,689,304
+2,691,834
 TOTAL TIER 1 JEWELS MINED
-79,772,750
+79,879,069
 NUMBER OF JEWEL MENTAR SLOTS
-27,595,516
+27,599,074
 TOTAL NO. OF TILES PURCHASED
-123,510,742
+123,534,849
 NO. OF T1 TILES PURCHASED
-67,196,206
+67,201,180
 NO. OF T2 TILES PURCHASED
-49,327,979
+49,332,116
 NO. OF T3 TILES PURCHASED
-6,986,557
+7,001,553
 MAX SUPPLY (MINUS BURNT TOKENS)
 984,421,148
 MINTED TOKENS
@@ -29,39 +29,35 @@ MINTED TOKENS
 BURNT TOKENS
 15,578,852
 E-THER COLLECTED PAST 24 HOURS
-1,751,990
+1,800,182
 BIDS MADE TOTAL
-2,089,808
+2,090,499
 BIDS REJECTED TOTAL
-369,666
+370,301
 TOTAL HOLOBUILDINGS BUILT
-1,208,764
+1,209,311
 NO. OF CYDROIDS
-2,541,645
+2,542,576
 NO. OF CIVILIANS
-1,599,132
+1,601,194
 NO. OF RAID COMMANDERS
-430,981
+431,825
 NO. OF E-THER RECKONERS
-435,162
+435,578
 NO. OF CYDROID TECHNICIANS
-447,323
+447,937
 NO. OF GEOLOGISTS
-285,666`
+285,854`;
 
 //console.log(scrap);
 
-
 // Si 1er élément = metrics = le remplacer par la date et l'heure actuelle au début
-
-
-
 
 // https://waytolearnx.com/2019/09/comment-afficher-la-date-et-lheure-actuelles-en-javascript.html
 let d = new Date();
-let date = d.getFullYear() + `/` + (d.getMonth()+1) + `/` + d.getDate();
+let date = d.getFullYear() + `/` + (d.getMonth() + 1) + `/` + d.getDate();
 let hours = d.getHours() + ":" + d.getMinutes();
-let fullDate = date+' '+hours;
+let fullDate = date + " " + hours;
 // console.log(fullDate);
 
 let dailyData;
@@ -74,66 +70,60 @@ dailyData = dailyData.replaceAll("-", "");
 
 // console.log(dailyData);
 
-
-
 // Calcul nombre d'éléments à indexer
 //console.log(dailyData.split(/\r\n|\r|\n/).length/2);
 
-
-
 // Traiter les données pour qu'elles correspondent au format data.json
 
-//console.log(dailyData); 
+//console.log(dailyData);
 // console.log(dailyData.split(/\r\n|\r|\n/));
 dailyData = dailyData.split(/\r\n|\r|\n/); // Passe le string en objet
 
-
-let tableau=[];
+let tableau = [];
 for (element in dailyData) {
-    // console.log(dailyData[element].charAt(0)); // premier caractère
-    // console.log(dailyData[element].charAt(dailyData[element].length-1)); // dernier caractère
+  // console.log(dailyData[element].charAt(0)); // premier caractère
+  // console.log(dailyData[element].charAt(dailyData[element].length-1)); // dernier caractère
 
+  // Si le dernier caractère est une lettre , ajouter ": " et remplacer " " par "_" et tout mettre entre guillements
+  if (
+    dailyData[element].charAt(dailyData[element].length - 1).match(/[a-z]/i)
+  ) {
+    dailyData[element] = dailyData[element].replaceAll(" ", "_");
+    dailyData[element] = dailyData[element] + `" : `; // Ajoute : à la fin de l'index
+    //console.log(dailyData[element]);
 
-    // Si le dernier caractère est une lettre , ajouter ": " et remplacer " " par "_" et tout mettre entre guillements
-    if (dailyData[element].charAt(dailyData[element].length-1).match(/[a-z]/i)) {
-        dailyData[element] = dailyData[element].replaceAll(" ", "_");
-        dailyData[element] = dailyData[element] + `" : `; // Ajoute : à la fin de l'index
-        //console.log(dailyData[element]);
-        
-        console.log(dailyData[element]);
+    console.log(dailyData[element]);
+  } else if (
+    dailyData[element].charAt(dailyData[element].length - 1).match(/[0-9]/)
+  ) {
+    // Si le dernier caractère est un nombre : rajoute l'élément à l'élément précédent et incorpore le tout dans un tableau
+    dailyData[element] =
+      `"` + dailyData[element - 1] + `"` + dailyData[element] + `"`;
 
-    } else if (dailyData[element].charAt(dailyData[element].length-1).match(/[0-9]/)) {
-        // Si le dernier caractère est un nombre : rajoute l'élément à l'élément précédent et incorpore le tout dans un tableau
-        dailyData[element] = `"` + dailyData[element-1] + `"` + dailyData[element] +`"`; 
-        
-        console.log(dailyData[element]);
-        tableau.push(dailyData[element]); // 
-    }
-    
-        //dailyData = dailyData.replaceAll(`\n`, ":");
+    console.log(dailyData[element]);
+    tableau.push(dailyData[element]); //
+  }
+
+  //dailyData = dailyData.replaceAll(`\n`, ":");
 }
 // console.log(tableau);
 // console.log(dailyData);
 
 let dailyDataJSON = "";
-for(element in tableau) {
-        dailyDataJSON = dailyDataJSON + tableau[element] + ", ";
+for (element in tableau) {
+  dailyDataJSON = dailyDataJSON + tableau[element] + ", ";
 
-// Supprimer la dernière virgule/dernier caractère
+  // Supprimer la dernière virgule/dernier caractère
 }
 // console.log(dailyDataJSON);
 //console.log(dailyDataJSON.charAt(dailyDataJSON.length-2));
 dailyDataJSON = dailyDataJSON.substring(0, dailyDataJSON.length - 2);
 console.log(dailyDataJSON);
 
-
-
 // Uploader les données dans data.json
 
-// Scraper 
+// Scraper
 // https://medium.com/data-scraper-tips-tricks/scraping-data-with-javascript-in-3-minutes-8a7cf8275b31
-
-
 
 /*
 // Récupérer les données Metrics sur https://earth2.io/
@@ -157,11 +147,6 @@ console.log(index.innerHTML);
 
 // document.execCommand('SaveAs',true,'.html');
 
-
-
-
 // Hébergement web
 // https://help.ovhcloud.com/csm/fr-web-hosting-multiple-websites-configuration?id=kb_article_view&sysparm_article=KB0052928
 // https://help.ovhcloud.com/csm/fr-web-hosting-publishing-website?id=kb_article_view&sysparm_article=KB0052788
-
-

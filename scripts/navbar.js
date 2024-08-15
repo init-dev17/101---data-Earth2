d6c8e711c689db77d87655f41d364853c66437b4
init-dev17 <dev@init17.fr>
2024-08-15 13:23:49 +0200

    sidebar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -13,6 +13,8 @@ burgerBtn.addEventListener("click", () => {
 // Si la classe .active n'est associé à #burgerBtn, l'ajouter, sinon la retirer
     menu.classList.toggle("active"); // Ajoute la classe .active au span #burgerBtn
 
+    // et changer l'icone :
+    // <i class="fa-solid fa-xmark"></i>
 
     //menu.classList.remove("active"); // Ajoute la classe .active au span #burgerBtn
 

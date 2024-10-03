507b1578435cc729a9a5ff1e34b4ad21a5eff153
init-dev17 <dev@init17.fr>
2024-10-03 09:54:00 +0200

    skinsJSON + statsUpdate03/10

---
+++ scripts/scriptSkins.js
@@ -0,0 +1,21 @@
+
+
+/*
+
+Skin Name
+Type:
+Description:
+Total Supply:
+Items Available:
+EcoSim Total:
+EcoSim Drop Chance:
+Base Item Required:
+Customisation:
+Can Be Resold:
+Future Price Reduction:
+Image:
+Video:
+
+*/
+
+

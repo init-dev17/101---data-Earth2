11370fd21ae2892af358176896a69d6691e0e713
init-dev17 <dev@init17.fr>
2024-10-07 07:54:48 +0200

    skins

--- scripts/scriptSkins.js
+++ scripts/scriptSkins.js
@@ -1,5 +1,56 @@
 
 
+// Lire JSON
+// https://developer.mozilla.org/fr/docs/Web/API/Fetch_API/Using_Fetch
+async function afficherDerniereDataJSON() {
+    const reponse = await fetch("./data.json");
+    const array = await reponse.json();
+    
+    lastUpdate.innerHTML = array[array.length - 1].DATE;
+}
+afficherDerniereDataJSON();
+
+
+
+
+
+fetch("./scripts/skins.json")
+  .then(function (response) {
+    if (response.ok == true) {
+      return response.json();
+    }
+})
+.then(function (data) {
+    console.log(data);
+
+
+    
+    function genererSkins(release){
+        for(release in data) {
+            console.log("MODELE : " + release);
+
+        
+            for(skin in data[release]) {
+                // console.log(skin);
+                console.log(data[release][skin].Skin_Name);
+                
+            }
+        }
+        
+    }
+
+    genererSkins();
+    // console.log(data.Murushierago[0].Type);
+    
+    
+
+
+
+});
+
+
+
+
 /*
 
 Skin Name
@@ -13,6 +64,7 @@ Base Item Required:
 Customisation:
 Can Be Resold:
 Future Price Reduction:
+Realeasing Price:
 Image:
 Video:
 

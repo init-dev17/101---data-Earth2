b05007e767f7790d7ccb8b923872c566fb491241
init-dev17 <dev@init17.fr>
2024-10-07 22:11:46 +0200

    skinsPage

--- scripts/scriptSkins.js
+++ scripts/scriptSkins.js
@@ -29,13 +29,64 @@ fetch("./scripts/skins.json")
         for(release in data) {
             console.log("MODELE : " + release);
 
+            const skins = document.querySelector(".skins");
+
+            const releaseName = document.createElement("h2");
+            releaseName.innerText = release;
+            skins.appendChild(releaseName);
+
+           
         
             for(skin in data[release]) {
                 // console.log(skin);
-                console.log(data[release][skin].Skin_Name);
+                // console.log(data[release][skin].Image);
+                // console.log(data[release][skin].Skin_Name);
                 
+
+                const element = document.createElement("div");
+                element.classList.add("skins-container");
+
+                const imgSkins = document.createElement("div");
+                imgSkins.classList.add("img-skins");
+                
+                const imgBtn = document.createElement("button");
+                imgBtn.innerText = "Play"
+                imgBtn.setAttribute("id","skin-btn");
+
+                const img = document.createElement("img");
+                img.src = "./assets/Skins/" + data[release][skin].Image;
+
+                const infoSkins = document.createElement("div");
+                infoSkins.classList.add("info-skins");
+
+                const paragraphe = document.createElement("span");
+                
+
+                skins.appendChild(element);
+                element.appendChild(imgSkins);
+                imgSkins.appendChild(img);
+                imgSkins.appendChild(imgBtn);
+                
+                element.appendChild(infoSkins);
+                // paragraphe.innerText = "Skin Name: " + data[release][skin].Skin_Name;
+                paragraphe.innerHTML = "Skin Name: " + data[release][skin].Skin_Name + "<br>" +
+                "Type: " + data[release][skin].Type + "<br>" +
+                "Description: " + data[release][skin].Description + "<br>" +
+                "Total Supply: " + data[release][skin].Total_Supply + "<br>" +
+                "Items Available: " + data[release][skin].Items_Available + "<br>" +
+                "EcoSim Total: " + data[release][skin].EcoSim_Total + "<br>" +
+                "EcoSim Drop Chance: " + data[release][skin].EcoSim_Drop_Chance + "<br>" +
+                "Base Item Required: " + data[release][skin].Base_Item_Required + "<br>" +
+                "Customisation: " + data[release][skin].Customisation + "<br>" +
+                "Can Be Resold: " + data[release][skin].Can_Be_Resold + "<br>" +
+                "Future Price Reduction: " + data[release][skin].Future_Price_Reduction + "<br>" +
+                "Realeasing Price: "+ data[release][skin].Price + "<br>";
+                infoSkins.appendChild(paragraphe);
+
+
             }
         }
+        console.log("./Skins/" + data[release][skin].Image);
         
     }
 

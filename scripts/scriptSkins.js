577da7e6e4369d552badb2d2076baa6cb0ede66e
init-dev17 <dev@init17.fr>
2024-10-07 23:27:17 +0200

    skinsFormat

--- scripts/scriptSkins.js
+++ scripts/scriptSkins.js
@@ -57,8 +57,8 @@ fetch("./scripts/skins.json")
                 const img = document.createElement("img");
                 img.src = "./assets/Skins/" + data[release][skin].Image;
 
-                const infoSkins = document.createElement("div");
-                infoSkins.classList.add("info-skins");
+                const infosSkins = document.createElement("div");
+                infosSkins.classList.add("infos-skins");
 
                 const paragraphe = document.createElement("span");
                 
@@ -66,24 +66,25 @@ fetch("./scripts/skins.json")
                 skins.appendChild(element);
                 element.appendChild(imgSkins);
                 imgSkins.appendChild(img);
-                imgSkins.appendChild(imgBtn);
                 
-                element.appendChild(infoSkins);
+                
+                element.appendChild(infosSkins);
                 // paragraphe.innerText = "Skin Name: " + data[release][skin].Skin_Name;
-                paragraphe.innerHTML = "Skin Name: " + data[release][skin].Skin_Name + "<br>" +
-                "Type: " + data[release][skin].Type + "<br>" +
-                "Description: " + data[release][skin].Description + "<br>" +
-                "Total Supply: " + data[release][skin].Total_Supply + "<br>" +
-                "Items Available: " + data[release][skin].Items_Available + "<br>" +
-                "EcoSim Total: " + data[release][skin].EcoSim_Total + "<br>" +
-                "EcoSim Drop Chance: " + data[release][skin].EcoSim_Drop_Chance + "<br>" +
-                "Base Item Required: " + data[release][skin].Base_Item_Required + "<br>" +
-                "Customisation: " + data[release][skin].Customisation + "<br>" +
-                "Can Be Resold: " + data[release][skin].Can_Be_Resold + "<br>" +
-                "Future Price Reduction: " + data[release][skin].Future_Price_Reduction + "<br>" +
-                "Realeasing Price: "+ data[release][skin].Price + "<br>";
-                infoSkins.appendChild(paragraphe);
-
+                paragraphe.innerHTML = "<u>Skin Name</u>: " + data[release][skin].Skin_Name + "<br>" + "<br>" +
+                "<u>Type</u>: " + data[release][skin].Type + "<br>" +
+                "<u>Description</u>: " + data[release][skin].Description + "<br>" +
+                "<u>Total Supply</u>: " + data[release][skin].Total_Supply + "<br>" +
+                "<u>Items Available</u>: " + data[release][skin].Items_Available + "<br>" +
+                "<u>EcoSim Total</u>: " + data[release][skin].EcoSim_Total + "<br>" +
+                "<u>EcoSim Drop Chance</u>: " + data[release][skin].EcoSim_Drop_Chance + "<br>" +
+                "<u>Base Item Required</u>: " + data[release][skin].Base_Item_Required + "<br>" +
+                "<u>Customisation</u>: " + data[release][skin].Customisation + "<br>" +
+                "<u>Can Be Resold</u>: " + data[release][skin].Can_Be_Resold + "<br>" +
+                "<u>Future Price Reduction</u>: " + data[release][skin].Future_Price_Reduction + "<br>" + "<br>" +
+                "<u>Release Date</u>: "+ data[release][skin].Release_Date + "<br>" +
+                "<u>Releasing Price</u>: "+ data[release][skin].Price + "<br>";
+                infosSkins.appendChild(paragraphe);
+                infosSkins.appendChild(imgBtn);
 
             }
         }
